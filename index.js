b07a844f0f60bd1d90e9c03479e985c215479f79
tylerCBD <tyler@creativebranddesign.co.uk>
2022-07-15 10:57:45 +0100

    bug fix for blend mode
    now works without additional CSS

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const cursorDot = ({
   const cur = { x: 0, y: 0, o: 0, d: diameter }
   const dot = document.createElement('div')
   const tim = easing / 15
-  dot.style = `position:fixed;top:0;left:0;border-radius:100%;pointer-events:none;opacity:0;z-index:${zIndex};height:${diameter}px;width:${diameter}px;background:${background};border:${borderWidth}px solid ${borderColor};mix-blend-mode:${blendMode};transition:background ${tim}s,border ${tim}s;will-change:transform`
+  dot.style = `--blend-mode:${blendMode};position:fixed;top:0;left:0;border-radius:100%;pointer-events:none;opacity:0;z-index:${zIndex};height:${diameter}px;width:${diameter}px;background:${background};border:${borderWidth}px solid ${borderColor};mix-blend-mode:var(--blend-mode);transition:background ${tim}s,border ${tim}s;will-change:transform`
 
   document.addEventListener('mousemove', e => {
     alt.x = e.clientX
